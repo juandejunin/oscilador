11755ab3c685d52f83ee7831c7e5ae006b2ff6d3
juandejunin <juandejunin75@gmail.com>
2024-04-05 07:41:19 +0200

    feat: value  input empty value  input range

--- js/oscilador.js
+++ js/oscilador.js
@@ -1,3 +1,43 @@
+// window.onload = function () {
+//   // Se asegura de que se utiliza el contexto de audio adecuado en todos los navegadores
+//   window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+//   // Inicialización de variables globales
+//   var contextoDeAudio = new AudioContext();
+//   var estaReproduciendo = false;
+//   var nodoFuente = null;
+//   var frecuencia = 220
+
+
+//   // Alterna la reproducción de un oscilador en el navegador
+//   function alternarOscilador() {
+//     var control = document.getElementById("controlDeFrecuenciaRango");
+//     frecuencia = control.value;
+//     console.log(frecuencia)
+//     if (estaReproduciendo) {
+//       // Detiene la reproducción y retorna
+//       nodoFuente.stop(0);
+//       nodoFuente = null;
+//       estaReproduciendo = false;
+//       return "Reproducir oscilador";
+//     }
+
+//     nodoFuente = contextoDeAudio.createOscillator();
+//     nodoFuente.frequency.value = frecuencia; // Frecuencia predeterminada: 440 Hz
+//     nodoFuente.connect(contextoDeAudio.destination);
+//     nodoFuente.start(0);
+//     estaReproduciendo = true;
+
+//     return "Detener";
+//   }
+
+//   // Agregar controlador de eventos de clic al botón "Play" para disparar el oscilador
+//   document.getElementById("playButton").addEventListener("click", function() {
+//     alternarOscilador();
+//   });
+// };
+
+
 window.onload = function () {
   // Se asegura de que se utiliza el contexto de audio adecuado en todos los navegadores
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -6,14 +46,19 @@ window.onload = function () {
   var contextoDeAudio = new AudioContext();
   var estaReproduciendo = false;
   var nodoFuente = null;
-  var frecuencia = 220
+  var frecuencia = 220;
 
+  // Función para actualizar el valor del input de texto con el valor del input de rango
+  function actualizarValorDeTexto(valor) {
+    document.getElementById("controlDeFrecuenciaTexto").value = valor;
+  }
 
   // Alterna la reproducción de un oscilador en el navegador
   function alternarOscilador() {
     var control = document.getElementById("controlDeFrecuenciaRango");
     frecuencia = control.value;
-    console.log(frecuencia)
+    actualizarValorDeTexto(frecuencia); // Actualiza el valor del input de texto
+
     if (estaReproduciendo) {
       // Detiene la reproducción y retorna
       nodoFuente.stop(0);
@@ -31,8 +76,17 @@ window.onload = function () {
     return "Detener";
   }
 
+  // Agregar controlador de eventos de input al input de rango para actualizar el valor del input de texto
+  document
+    .getElementById("controlDeFrecuenciaRango")
+    .addEventListener("input", function (event) {
+      var valor = event.target.value;
+      actualizarValorDeTexto(valor); // Actualiza el valor del input de texto
+    });
+
   // Agregar controlador de eventos de clic al botón "Play" para disparar el oscilador
-  document.getElementById("playButton").addEventListener("click", function() {
+  document.getElementById("playButton").addEventListener("click", function () {
     alternarOscilador();
   });
 };
+
