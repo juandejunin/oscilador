78973bfc7f531c29c1b86c706681858963f87513
juandejunin <juandejunin75@gmail.com>
2024-04-06 06:51:48 +0200

    feat: actualizar el oscilador cada vez que se mueve el input range

--- js/oscilador.js
+++ js/oscilador.js
@@ -46,7 +46,7 @@ window.onload = function () {
   var contextoDeAudio = new AudioContext();
   var estaReproduciendo = false;
   var nodoFuente = null;
-  var frecuencia = 220;
+  var frecuencia = 440;
 
   // Función para actualizar el valor del input de texto con el valor del input de rango
   function actualizarValorDeTexto(valor) {
@@ -55,14 +55,9 @@ window.onload = function () {
 
   // Alterna la reproducción de un oscilador en el navegador
   function alternarOscilador() {
-    var control = document.getElementById("controlDeFrecuenciaRango");
-    frecuencia = control.value;
-    actualizarValorDeTexto(frecuencia); // Actualiza el valor del input de texto
-
     if (estaReproduciendo) {
       // Detiene la reproducción y retorna
       nodoFuente.stop(0);
-      nodoFuente = null;
       estaReproduciendo = false;
       return "Reproducir oscilador";
     }
@@ -76,17 +71,26 @@ window.onload = function () {
     return "Detener";
   }
 
-  // Agregar controlador de eventos de input al input de rango para actualizar el valor del input de texto
+  // Agregar controlador de eventos de input al input de rango para actualizar el valor del input de texto y el sonido
   document
     .getElementById("controlDeFrecuenciaRango")
     .addEventListener("input", function (event) {
       var valor = event.target.value;
+      frecuencia = valor;
       actualizarValorDeTexto(valor); // Actualiza el valor del input de texto
+      if (estaReproduciendo) {
+        // Si se está reproduciendo, detiene el sonido actual y lo vuelve a iniciar con la nueva frecuencia
+        nodoFuente.stop(0);
+        nodoFuente = contextoDeAudio.createOscillator();
+        nodoFuente.frequency.value = frecuencia;
+        nodoFuente.connect(contextoDeAudio.destination);
+        nodoFuente.start(0);
+      }
     });
 
   // Agregar controlador de eventos de clic al botón "Play" para disparar el oscilador
   document.getElementById("playButton").addEventListener("click", function () {
-    alternarOscilador();
+    document.getElementById("playButton").textContent = alternarOscilador();
   });
 };
 
